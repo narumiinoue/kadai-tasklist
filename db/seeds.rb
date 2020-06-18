4c7cde3ccc772a6bba124a6bafc2d34d2eab852d
narumi.inoue <narumi.chatateba@gmail.com>
2020-06-18 12:01:09 +0000

    add db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Task.create(content: 'content1', status: 'test status 1')
-Task.create(content: 'content2', status: 'test status 2')
-Task.create(content: 'content3', status: 'test status 3')
+(1..100).each do |number|
+  Task.create!(content: 'test content ' + number.to_s, status: 'status ' + number.to_s)
+end
