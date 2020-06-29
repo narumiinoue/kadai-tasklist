ad38f89f340c6b3e7c0274e023c48f1caef0f180
narumi.inoue <narumi.chatateba@gmail.com>
2020-06-29 07:40:11 +0000

    modified controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,10 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy] 
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :edit, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy] 
   
   def index
-      if logged_in?
-        @tasks = current_user.tasks.all.page(params[:page]).per(15)
-      end
+      @tasks = current_user.tasks.all.page(params[:page]).per(15)
   end
 
   def show
